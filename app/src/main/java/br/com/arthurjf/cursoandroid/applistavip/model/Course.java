815579d07425356b09547c0ee662c877bdd0a6b1
arthurjf <arthur.310100@hotmail.com>
2023-03-25 17:46:54 -0300

    Teste com listener do botao

--- app/src/main/java/br/com/arthurjf/cursoandroid/applistavip/model/Course.java
+++
@@ -1,4 +0,0 @@
-package br.com.arthurjf.cursoandroid.applistavip.model;
-
-public class Course {
-}
--- app/src/main/java/br/com/arthurjf/cursoandroid/applistavip/view/MainActivity.kt
+++ app/src/main/java/br/com/arthurjf/cursoandroid/applistavip/view/MainActivity.kt
@@ -2,11 +2,21 @@ package br.com.arthurjf.cursoandroid.applistavip.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import br.com.arthurjf.cursoandroid.applistavip.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (findViewById<Button>(R.id.buttonFinish)).setOnClickListener{
+            val userName = findViewById<EditText>(R.id.editPersonName).text.toString();
+            val text = "Olá " + userName.trim() + " :)"
+            Toast.makeText(applicationContext, text, Toast.LENGTH_LONG).show()
+        }
     }
 }
